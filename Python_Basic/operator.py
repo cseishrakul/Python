7dda1a2ae03a2fe481b7afebe2ed90fa799ec29f
Ishrakul Islam <ishrakul236@gmail.com>
2021-07-30 21:05:37 +0600

    Python Basic

---
+++ Python_Basic/operator.py
@@ -0,0 +1,17 @@
+#Add
+print(2 + 7)
+#sub
+print(2-7)
+#into
+print(2*7)
+#div
+print(2/7)
+#power
+print(2**3)
+#interger number
+print(2//7)
+#String
+print('Python')
+print('Python ' + 'is ' + 'awesome')
+print('python '*2)
+print(8+.2)
---
+++ Python_Basic/variable.py
@@ -0,0 +1,6 @@
+a = 2
+print(a)
+a=4
+print(a)
+b = 3
+print(a+b)
